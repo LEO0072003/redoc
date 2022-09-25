c42b91f1180429e8c5b4df71c3818b271f7ab04a
LEO0072003 <106365961+LEO0072003@users.noreply.github.com>
2022-09-25 07:51:39 +0530

    login auth modified

--- rec/urls.py
+++ rec/urls.py
@@ -1,7 +1,15 @@
+from tkinter import N
 from django.urls import path
 from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/login',views.login, name='login'),
+
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutPage, name='logout'),
+    path('register_user/', views.registerUser, name='register'),
+
+    path('user_profile/<str:pk>/', views.userProfile, name='profile'),
+    path('appointment/<str:pk>', views.appointmentDetails, name='appointment'),
+
 ]
